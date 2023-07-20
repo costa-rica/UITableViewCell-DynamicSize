082bd95b1fa23824fe374eb279833858c655205f
costa-rica <nrodrig1@gmail.com>
2023-07-20 11:50:31 +0200

    PostCell added commentsView - this is all he comments for each post - very dynamic-y

--- UITableViewCell-Dynamic01/Utilities.swift
+++ UITableViewCell-Dynamic01/Utilities.swift
@@ -41,3 +41,37 @@ func sizeLabel(lbl:UILabel)  -> UILabel{
     
     return lbl
 }
+
+// Crops from both sides evenly no aspect ratio adjustment
+func cropImage(image: UIImage, width: CGFloat) -> UIImage? {
+    let originalSize = image.size
+    let originalWidth = originalSize.width
+    let originalHeight = originalSize.height
+    
+    // Calculate the desired height based on the original aspect ratio
+    let scaleFactor = width / originalWidth
+    let croppedHeight = originalHeight * scaleFactor
+    
+    // Calculate the crop rect
+    let cropRect = CGRect(x: (originalWidth - width) / 2, y: 0, width: width, height: croppedHeight)
+    
+    // Perform the crop operation
+    if let croppedImage = image.cgImage?.cropping(to: cropRect) {
+        return UIImage(cgImage: croppedImage)
+    }
+    
+    return nil
+}
+
+func createDividerLine(thicknessOfLine:CGFloat) -> UIImageView{
+    let screenWidth = UIScreen.main.bounds.width
+    let lineImage = UIImage(named: "line01")
+    
+    let lineImageScreenWidth = cropImage(image: lineImage!, width: screenWidth)
+    let cropThin = CGRect(x:0,y:0,width:screenWidth,height:thicknessOfLine)
+    let lineImageScreenWidthThin = lineImageScreenWidth?.cgImage?.cropping(to: cropThin)
+    let imageViewLine = UIImageView(image: UIImage(cgImage: lineImageScreenWidthThin!))
+//    imageViewLine.layer.opacity = 0.6
+    imageViewLine.layer.opacity = 0.1
+    return imageViewLine
+}
--- UITableViewCell-Dynamic01/Views/PostCell.swift
+++ UITableViewCell-Dynamic01/Views/PostCell.swift
@@ -22,6 +22,16 @@ class PostCell: UITableViewCell {
     var stckVwUserInteractionHeight:CGFloat!
     var likeView:LikeView!
     var commentView:CommentView!
+    var lineImageImageView02: UIImageView!
+    
+    //comments stack
+    var stckVwComments: UIStackView?
+    var stckVwCommentsHeight: CGFloat?
+    var dictCommentsDate: [String:UILabel]?
+    var dictCommentsUsername: [String:UILabel]?
+    var dictCommentsText: [String:UILabel]?
+    var dictCommentsVw: [String:UIView]?
+    
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -35,10 +45,25 @@ class PostCell: UITableViewCell {
         stckVwPostCell.removeFromSuperview()
         lblDate.removeFromSuperview()
         lblPostText?.removeFromSuperview()
-        
+        lineImageImageView01.removeFromSuperview()
         stckVwUserInteraction.removeFromSuperview()
         likeView.removeFromSuperview()
         commentView.removeFromSuperview()
+        
+        lineImageImageView02.removeFromSuperview()
+
+        if let unwrapped_comments = post.comments {
+            for comment_element in unwrapped_comments{
+                if let commentId = comment_element["comment_id"]{
+                    dictCommentsDate?["\(post.post_id!), \(commentId)"]?.removeFromSuperview()
+                    dictCommentsUsername?["\(post.post_id!), \(commentId)"]?.removeFromSuperview()
+                    dictCommentsText?["\(post.post_id!), \(commentId)"]?.removeFromSuperview()
+                    dictCommentsVw!["\(post.post_id!), \(commentId)"]?.removeFromSuperview()
+                }
+            }
+        }
+        
+        stckVwComments?.removeFromSuperview()
     }
     
     func configure(with post: Post) {
@@ -47,7 +72,10 @@ class PostCell: UITableViewCell {
         setup_lblDate()
         setup_lblUsername()
         setup_lblPostText()
+        setup_line01()
         setup_userInteractionStackView()
+        setup_line02()
+        setup_commentsView()
     }
     
     func setup_stckVwPostCell(){
@@ -91,7 +119,11 @@ class PostCell: UITableViewCell {
             stckVwPostCell.addArrangedSubview(lblPostText!)
         }
     }
-    
+    func setup_line01(){
+        lineImageImageView01 = createDividerLine(thicknessOfLine: 1.0)
+        stckVwPostCell.addArrangedSubview(lineImageImageView01)
+//        post.cell_height = post.cell_height + lineImageImageView01.image!.size.height
+    }
     func setup_userInteractionStackView(){
         stckVwUserInteraction.axis = .horizontal
 //        stckVwUserInteraction.distribution = .equalSpacing
@@ -125,5 +157,105 @@ class PostCell: UITableViewCell {
 //        commentView.backgroundColor = .cyan
 //        likeView.backgroundColor = .gray
     }
+    func setup_line02(){
+        lineImageImageView02 = createDividerLine(thicknessOfLine: 1.5)
+        stckVwPostCell.addArrangedSubview(lineImageImageView02)
+//        post.cell_height = post.cell_height + lineImageImageView02.image!.size.height
+    }
     
+    func setup_commentsView(){
+
+        stckVwComments =  UIStackView()
+        stckVwComments!.translatesAutoresizingMaskIntoConstraints = false
+        stckVwComments!.axis = .vertical
+        stckVwPostCell.addArrangedSubview(stckVwComments!)
+        dictCommentsDate = [String:UILabel]()
+        dictCommentsUsername = [String:UILabel]()
+        dictCommentsText = [String:UILabel]()
+        dictCommentsVw = [String:UIView]()
+        
+        
+        if let unwrapped_comments = post.comments {
+            stckVwCommentsHeight = 0.0
+            stckVwComments?.spacing = heightFromPct(percent: 0.5)
+            for comment_element in unwrapped_comments{
+                if let commentId = comment_element["comment_id"]{
+                    dictCommentsDate!["\(post.post_id!), \(commentId)"] = UILabel()
+                    dictCommentsUsername!["\(post.post_id!), \(commentId)"] = UILabel()
+                    dictCommentsText!["\(post.post_id!), \(commentId)"] = UILabel()
+                    dictCommentsVw!["\(post.post_id!), \(commentId)"] = UIView()
+                    
+                    if let unwp_lblDate = dictCommentsDate!["\(post.post_id!), \(commentId)"],
+                       let unwp_lblUsername = dictCommentsUsername!["\(post.post_id!), \(commentId)"],
+                       let unwp_lblText = dictCommentsText!["\(post.post_id!), \(commentId)"],
+                        let unwp_vw = dictCommentsVw!["\(post.post_id!), \(commentId)"] {
+                        
+                        unwp_vw.backgroundColor = .gray
+                        unwp_vw.layer.cornerRadius = 10
+                        
+                        unwp_vw.accessibilityIdentifier = "unwp_vw: \(post.post_id!), \(commentId)"
+
+                        unwp_lblDate.text = comment_element["date"]
+                        unwp_lblDate.accessibilityIdentifier = "unwp_lblDate: \(post.post_id!), \(commentId)"
+//                        let date_obj = convertStringToDate(date_string: post.date_for_sorting_ios)
+//                        let formattedDateString = DateFormatter.localizedString(from: date_obj, dateStyle: .medium, timeStyle: .short)
+//                        lblDate.text = formattedDateString
+                        unwp_lblDate.semanticContentAttribute = .forceRightToLeft
+                        unwp_lblDate.translatesAutoresizingMaskIntoConstraints = false
+                        let _ = sizeLabel(lbl: unwp_lblDate)
+                        unwp_vw.addSubview(unwp_lblDate)
+                        unwp_lblDate.topAnchor.constraint(equalTo: unwp_vw.topAnchor, constant: heightFromPct(percent: 1)).isActive=true
+                        unwp_lblDate.trailingAnchor.constraint(equalTo: unwp_vw.trailingAnchor).isActive=true
+                        unwp_lblDate.leadingAnchor.constraint(equalTo: unwp_vw.leadingAnchor).isActive=true
+                        
+                        unwp_lblUsername.text = comment_element["username"]
+                        unwp_lblUsername.translatesAutoresizingMaskIntoConstraints=false
+                        let _ = sizeLabel(lbl: unwp_lblUsername)
+                        unwp_lblUsername.font = unwp_lblUsername.font.withSize(12.0)
+                        unwp_vw.addSubview(unwp_lblUsername)
+                        unwp_lblUsername.topAnchor.constraint(equalTo: unwp_lblDate.bottomAnchor, constant: heightFromPct(percent: 1)).isActive=true
+                        unwp_lblUsername.leadingAnchor.constraint(equalTo: unwp_vw.leadingAnchor, constant: widthFromPct(percent: 2)).isActive=true
+                        unwp_lblUsername.trailingAnchor.constraint(equalTo: unwp_vw.trailingAnchor).isActive=true
+                        
+                        unwp_lblText.text = comment_element["comment_text"]!
+                        unwp_lblText.numberOfLines = 0
+                        unwp_lblText.translatesAutoresizingMaskIntoConstraints=false
+                        let _ = sizeLabel(lbl: unwp_lblText)
+                        unwp_vw.addSubview(unwp_lblText)
+                        stckVwComments!.addArrangedSubview(unwp_vw)
+                        unwp_lblText.topAnchor.constraint(equalTo: unwp_lblUsername.topAnchor, constant: heightFromPct(percent: 2)).isActive=true
+                        unwp_lblText.trailingAnchor.constraint(equalTo: unwp_vw.trailingAnchor, constant: widthFromPct(percent: -2)).isActive=true
+                        unwp_lblText.bottomAnchor.constraint(equalTo: unwp_vw.bottomAnchor, constant: heightFromPct(percent: -2)).isActive=true
+                        unwp_lblText.leadingAnchor.constraint(equalTo: unwp_vw.leadingAnchor, constant: widthFromPct(percent: 2)).isActive=true
+                        
+//                        post.cell_height = post.cell_height + unwp_lblText.frame.size.height + heightFromPct(percent: 4) + heightFromPct(percent: 1) + unwp_lblDate.frame.size.height + heightFromPct(percent: 2) + unwp_lblUsername.frame.size.height + heightFromPct(percent: 2)
+//                        stckVwCommentsHeight = stckVwCommentsHeight! + unwp_lblText.frame.size.height + heightFromPct(percent: 4) + unwp_lblDate.frame.size.height + heightFromPct(percent: 2) + unwp_lblUsername.frame.size.height + heightFromPct(percent: 2)
+                    }
+                }
+            }
+            print("-------------------------")
+            print("likeView for post: \(post.post_id!)")
+            print("lblLikeCount size: \(likeView.lblLikeCount.frame.size)")
+            print("btnThumbsUp size: \(likeView.btnThumbsUp.imageView!.image!.size)")
+//            print("stckVwCommentsHeight: \(stckVwCommentsHeight!)")
+            
+//            if post.post_id == "37" {
+//                post.cell_height = post.cell_height + 20
+//                print("* added height *")
+////                stckVwComments!.layoutIfNeeded()
+//            }
+            
+            print("stckVwComment.frame.height: \(stckVwComments!.frame.size.height)")
+//            print("stckVwCommentsHeight: \(stckVwCommentsHeight!)")
+            
+            
+            print("-----------")
+//            post.cell_height = post.cell_height + stckVwComments!.frame.size.height
+        } // if let unwrapped_comments = post.comments
+        
+//        stckVwCommentsTest?.heightAnchor.constraint(equalToConstant: )
+//        setup_btnPostDiagnostics()
+        
+
+    }
 }
