2d5a5487a450aa588fb677c3e725753e85904ebd
costa-rica <nrodrig1@gmail.com>
2023-07-20 10:07:46 +0200

    Initial Commit

---
+++ UITableViewCell-Dynamic01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableViewCell-Dynamic01
+//
+//  Created by Nick Rodriguez on 20/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
